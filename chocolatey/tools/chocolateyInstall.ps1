568b44cbb01d1f9bdfb36af6c6f91528ab99383e
Ellery Newcomer <ellery-newcomer@utulsa.edu>
2014-07-17 15:46:32 -0400

    scripts need to run as admin

--- chocolatey/tools/chocolateyInstall.ps1
+++ chocolatey/tools/chocolateyInstall.ps1
@@ -1,27 +1,53 @@
-﻿$gnuradio = "C:\Program Files (x86)\gnuradio"
-$fosphor_base = "$($env:ChocolateyInstall)\lib\gnuradio-fosphor.1.0.0"
-$bindir = "$fosphor_base\bin"
-$pydir = "$fosphor_base\py-fosphor"
-$blocksdir = "$fosphor_base\blocks"
+﻿
 
 
-Function mkdirp {
-    param ( [String] $path)
-    if (-not (test-path $path -pathtype container)) {
-        mkdir $path
+
+Function Escape {
+    param(
+        $script
+    );
+    $result = $script -replace "`"", "`"`"`""
+    $result
+}
+
+Function RunAsAdmin {
+    param(
+        [ScriptBlock] $script
+    );
+
+    if(Test-ProcessAdminRights) {
+        . $script
+    }else{
+        start-process -filepath powershell -argument @("-NoProfile", "-ExecutionPolicy", "Bypass", 
+            "-Command", (Escape $script)) -verb "runas"
     }
 }
 
-foreach($file in (ls $bindir)) {
-    cp "$($file.FullName)" "$gnuradio\bin"
-}
 
-$pyfosphor = "$gnuradio\lib\site-packages\gnuradio\fosphor\"
-mkdirp $pyfosphor
-foreach($file in (ls $pydir)) {
-    cp "$($file.FullName)" "$pyfosphor"
-}
-foreach($file in (ls $blocksdir)) {
-    cp "$($file.FullName)" "$gnuradio\share\gnuradio\grc\blocks"
-}
+RunAsAdmin {
+    $gnuradio = "C:\Program Files (x86)\gnuradio"
+    $fosphor_base = "$($env:ChocolateyInstall)\lib\gnuradio-fosphor.1.0.0"
+    $bindir = "$fosphor_base\bin"
+    $pydir = "$fosphor_base\py-fosphor"
+    $blocksdir = "$fosphor_base\blocks"
 
+    Function mkdirp {
+        param ( [String] $path)
+            if (-not (test-path $path -pathtype container)) {
+                mkdir $path
+            }
+    }
+    foreach($file in (ls $bindir)) {
+        cp "$($file.FullName)" "$gnuradio\bin"
+    }
+
+    $pyfosphor = "$gnuradio\lib\site-packages\gnuradio\fosphor\"
+        mkdirp $pyfosphor
+        foreach($file in (ls $pydir)) {
+            cp "$($file.FullName)" "$pyfosphor"
+        }
+    foreach($file in (ls $blocksdir)) {
+        cp "$($file.FullName)" "$gnuradio\share\gnuradio\grc\blocks"
+    }
+
+}
--- chocolatey/tools/chocolateyUninstall.ps1
+++ chocolatey/tools/chocolateyUninstall.ps1
@@ -1,17 +1,40 @@
-$gnuradio = "C:\Program Files (x86)\gnuradio"
-$fosphor_base = "$($env:ChocolateyInstall)\lib\gnuradio-fosphor.1.0.0"
-$bindir = "$fosphor_base\bin"
-$pydir = "$fosphor_base\py-fosphor"
-$blocksdir = "$fosphor_base\blocks"
-
-foreach($file in (ls $bindir)) {
-    rm "$gnuradio\bin\$($file.Name)"
+Function Escape {
+    param(
+        $script
+    );
+    $result = $script -replace "`"", "`"`"`""
+    $result
 }
 
-foreach($file in (ls $pydir)) {
-    rm "$gnuradio\lib\site-packages\gnuradio\fosphor\$($file.Name)"
+Function RunAsAdmin {
+    param(
+        [ScriptBlock] $script
+    );
+
+    if(Test-ProcessAdminRights) {
+        . $script
+    }else{
+        start-process -filepath powershell -argument @("-NoProfile", "-ExecutionPolicy", "Bypass", 
+            "-Command", (Escape $script)) -verb "runas"
+    }
 }
-foreach($file in (ls $blocksdir)) {
-    rm "$gnuradio\share\gnuradio\grc\blocks\$($file.Name)"
+
+RunAsAdmin {
+    $gnuradio = "C:\Program Files (x86)\gnuradio"
+    $fosphor_base = "$($env:ChocolateyInstall)\lib\gnuradio-fosphor.1.0.0"
+    $bindir = "$fosphor_base\bin"
+    $pydir = "$fosphor_base\py-fosphor"
+    $blocksdir = "$fosphor_base\blocks"
+
+    foreach($file in (ls $bindir)) {
+        rm "$gnuradio\bin\$($file.Name)"
+    }
+
+    foreach($file in (ls $pydir)) {
+        rm "$gnuradio\lib\site-packages\gnuradio\fosphor\$($file.Name)"
+    }
+    foreach($file in (ls $blocksdir)) {
+        rm "$gnuradio\share\gnuradio\grc\blocks\$($file.Name)"
+    }
 }
 
